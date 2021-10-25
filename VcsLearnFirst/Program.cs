570123ec7eb6a139143282c19553ca6593f43919
tugolukov <tugolukov.obn@gmail.com>
2021-10-25 11:37:13 +0300

    Поменяли сообщение в консоль

--- VcsLearnFirst/Program.cs
+++ VcsLearnFirst/Program.cs
@@ -6,7 +6,7 @@ namespace VcsLearnFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Created from development");
         }
     }
 }
